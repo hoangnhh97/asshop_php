6a84b17efb776c2287657018a8c1ba8f1365b49d
Hoàng Nguyễn <44421616+hoangnhh140697@users.noreply.github.com>
2019-11-23 22:08:11 +0700

    Add slider to home page

--- views/v_home.php
+++ views/v_home.php
@@ -3,6 +3,32 @@
 
 ?>
 
+<section id="slider">
+    <div class="owl-carousel owl-theme">
+        <div class="item">
+            <div class="item-banner">
+                <img src="./assets/images/banner-2.jpg"/>
+            </div>
+            <div class="item-text">
+                <h3>Arrival Offer Available <br/> <span>Upto 40% Flat</span></h3>
+                <p>Discount On Women's Every Items <br/> <span>Stay Tuned For Sale Date</span></p>
+                
+                
+            </div>
+        </div>
+        <div class="item">
+            <div class="item-banner">
+                <img src="./assets/images/banner-2.jpg"/>
+            </div>
+            <div class="item-text">
+                <h3>Arrival Offer Available <br/> <span>Upto 40% Flat</span></h3>
+                <p>Discount On Women's Every Items <br/> <span>Stay Tuned For Sale Date</span></p>
+                
+                
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php 
     include_once("./views/shared/v_footer.php");
