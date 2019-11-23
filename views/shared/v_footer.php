60baf46feb398912e466a3225f1e2fa1220960c0
Hoàng Nguyễn <44421616+hoangnhh140697@users.noreply.github.com>
2019-11-23 21:59:08 +0700

    Update Footer

--- views/shared/v_footer.php
+++ views/shared/v_footer.php
@@ -1,8 +1,26 @@
         </div>
 
         <footer>
-            <div class="container">
-                <h1>This is Footer</h1>
+            <div class="footer">
+                <div class="container">
+                    <h1>This is Footer</h1>
+                </div>
+            </div>
+            <div class="copyright pt-3 pb-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p class="text-white">Copyright &copy; 2019 XinChaoCoder</p>
+                        </div>
+                        <div class="col-md-6">
+                            <ul class="list-payment-sprite text-right">
+                                <li><span class="method-1"></span></li>
+                                <li><span class="method-2"></span></li>
+                                <li><span class="method-3"></span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </footer>
         
