9678c184aeda32c0beffc473f73f981202042ebd
Hoàng Nguyễn <44421616+hoangnhh140697@users.noreply.github.com>
2019-11-25 08:37:40 +0700

    Add list cate and ads block at home page

--- views/v_home.php
+++ views/v_home.php
@@ -12,8 +12,6 @@
             <div class="item-text">
                 <h3>Arrival Offer Available <br/> <span>Upto 40% Flat</span></h3>
                 <p>Discount On Women's Every Items <br/> <span>Stay Tuned For Sale Date</span></p>
-                
-                
             </div>
         </div>
         <div class="item">
@@ -23,8 +21,48 @@
             <div class="item-text">
                 <h3>Arrival Offer Available <br/> <span>Upto 40% Flat</span></h3>
                 <p>Discount On Women's Every Items <br/> <span>Stay Tuned For Sale Date</span></p>
-                
-                
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section id="list-cate">
+    <div class="container">
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <h1></h1>
+            </div>
+            <div class="item">
+                <h1>Cate2</h1>
+            </div>
+            <div class="item">
+                <h1>Cate2</h1>
+            </div>
+            <div class="item">
+                <h1>Cate2</h1>
+            </div>
+            <div class="item">
+                <h1>Cate2</h1>
+            </div>
+            <div class="item">
+                <h1>Cate2</h1>
+            </div>
+            <div class="item">
+                <h1>Cate2</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="ads-block-sec">
+    <div class="container">
+        <div class="row">
+            <div class="ads-block ads-block-1 col-md-6">
+                ADS1
+            </div>
+            <div class="ads-block ads-block-2 col-md-6">
+                ADS2
             </div>
         </div>
     </div>
